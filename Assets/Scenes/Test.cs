a1d72c85342e883425ccab9815b95eb3e354bd5e
ybwork-cn <34906711+ybwork-cn@users.noreply.github.com>
2024-03-31 12:00:50 +0800

    增加示例场景

---
+++ Assets/Scenes/Test.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ybwork.Assets;
+
+public class Test : MonoBehaviour
+{
+    async void Start()
+    {
+        await AssetMgr.InitAsync();
+        AssetMgr.SetDefaultGroup("DefaultPackage");
+        Instantiate(AssetMgr.LoadAssetSync<GameObject>("New Prefab"));
+    }
+}
