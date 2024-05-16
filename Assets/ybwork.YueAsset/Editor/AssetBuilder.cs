e7377f27a5cdd8959d9b607ecd08348b2c4fbef4
ybwork-cn <34906711+ybwork-cn@users.noreply.github.com>
2024-05-16 22:52:42 +0800

    生成带有文件大小和文件哈希的目录文件

--- Assets/ybwork.YueAsset/Editor/AssetBuilder.cs
+++ Assets/ybwork.YueAsset/Editor/AssetBuilder.cs
@@ -1,4 +1,5 @@
 ﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -7,18 +8,25 @@ using UnityEngine;
 
 namespace ybwork.Assets.Editor
 {
+    class BundleGroupInfo
+    {
+        public long Size;
+        public string Hash;
+    }
+
+
     internal static class AssetBuilder
     {
         internal static void BuildAssetBundle(AssetCollectorData collectorData)
         {
-            string outputPath = EditorUtility.SaveFolderPanel("保存到", "", "");
-            if (!Directory.Exists(outputPath))
-                return;
+            string outputPath = Path.Combine(Environment.CurrentDirectory, "YueAssets");
 
             Debug.Log("开始打Windows资源包");
-            Directory.Delete(outputPath, true);
+            if (Directory.Exists(outputPath))
+                Directory.Delete(outputPath, true);
             Directory.CreateDirectory(outputPath);
 
+            Dictionary<string, Dictionary<string, BundleGroupInfo>> packgeInfos = new();
             foreach (AssetCollectorPackageData packageData in collectorData.Packages)
             {
                 List<AssetBundleBuild> bundles = new List<AssetBundleBuild>();
@@ -33,12 +41,28 @@ namespace ybwork.Assets.Editor
                 }
                 string path = Path.Combine(outputPath, packageData.PackageName);
                 Directory.CreateDirectory(path);
-                BuildPipeline.BuildAssetBundles(path, bundles.ToArray(), BuildAssetBundleOptions.None, collectorData.BuildTarget);
+                AssetBundleManifest assetBundleManifest = BuildPipeline.BuildAssetBundles(path, bundles.ToArray(), BuildAssetBundleOptions.None, collectorData.BuildTarget);
                 Debug.Log($"{path}打包完成");
+
+
+                Dictionary<string, BundleGroupInfo> groupInfos = new();
+                foreach (string group in assetBundleManifest.GetAllAssetBundles())
+                {
+                    groupInfos.Add(group, new BundleGroupInfo
+                    {
+                        Hash = assetBundleManifest.GetAssetBundleHash(group).ToString(),
+                        Size = new FileInfo(Path.Combine(outputPath, packageData.PackageName, group)).Length,
+                    });
+                }
+                packgeInfos.Add(packageData.PackageName, groupInfos);
             }
 
             string aliasMap = JsonConvert.SerializeObject(collectorData.GetAssets(), Formatting.Indented);
             File.WriteAllText(Path.Combine(outputPath, "alias.json"), aliasMap);
+
+            string catalog = JsonConvert.SerializeObject(packgeInfos, Formatting.Indented);
+            File.WriteAllText(Path.Combine(outputPath, "catalog.json"), catalog);
+
             Debug.Log("资源包 打包完成");
         }
     }
